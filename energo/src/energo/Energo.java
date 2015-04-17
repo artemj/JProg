64e6cda04f38020bc2b86aeee9c284a280cec715
artem <artem.gladyshuk@gmail.com>
2015-04-17 16:17:22 +0300

    aaaa

--- energo/src/energo/Energo.java
+++ energo/src/energo/Energo.java
@@ -14,35 +14,32 @@ public class Energo {
     private static File file = new File("D://энерго.txt");
     
     public static String read(String fileName) throws FileNotFoundException {
-    StringBuilder sb = new StringBuilder();
-    exists(fileName);
-    try {
-        BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+        StringBuilder sb = new StringBuilder();
+        exists(fileName);
         try {
-            String s;
-            while ((s = in.readLine()) != null) {
-                sb.append(s);
-                sb.append("\n");
+            BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+            try {
+                String s;
+                while ((s = in.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            } finally {
+                in.close();
             }
-        } finally {
-            in.close();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
         }
-    } catch(IOException e) {
-        throw new RuntimeException(e);
+        return sb.toString();
     }
-    return sb.toString();
-}
 
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Integer> ob = new ArrayList<>();
         int a;
         int n = 0;
-    String ss = Energo.read("D://энерго.txt");
-    String sNew = ss.replace('\n',' ').replace('\t', ' ');
+        String ss = Energo.read("D://энерго.txt");
+        String sNew = ss.replace('\n',' ').replace('\t', ' ');
         String sMas[] = sNew.split(" ");
-//        for (int i=0; i<sMas.length;i++){
-//            System.out.println(sMas[i]);            
-//        }
         for (int i=0; i<sMas.length; i+=3){
             if(i+3<sMas.length){
                 if(sMas[i].equals(sMas[i+3])){
@@ -52,14 +49,14 @@ public class Energo {
                 else{    
                     a = Integer.valueOf(sMas[i+2]);
                     n += a;
-                   ob.add(n);
+                    ob.add(n);
                     n = 0; 
                 }
             }
             else{
                 a = Integer.valueOf(sMas[i+2]);
-                    n += a;
-                   ob.add(n);
+                n += a;
+                ob.add(n);
             }
         }
         int razmer = 0;
