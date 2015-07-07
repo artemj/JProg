69d527a6daa85ef48b2e7142cd99772eb90ecc15
artem <artem.gladyshuk@gmail.com>
2015-07-07 14:33:48 +0300

    words v 1.3

--- words/src/words/Words.java
+++ words/src/words/Words.java
@@ -2,17 +2,8 @@ package words;
 
 import java.util.*;
 
-public class Words {
+public class Words {  
 
-    public static void main(String[] args) {
-        Map<String, Integer> res1 = map(str1);
-        Map<String, Integer> res2 = map(str2);
-        union(res1,res2);
-    }
-    
-    private static final String str1 = "mam dad father mam father mam mam dad sis father mam father mam";
-    private static final String str2 = "mam dad father mam bob dad";
-    
     public static Map<String, Integer> map(String str){
         String strArr[] = str.split(" ");
         Map<String, Integer> map = new HashMap<>();
@@ -29,42 +20,24 @@ public class Words {
         return map;
     }
     
-    private static Map<String, Integer> map1;
-    private static Map<String, Integer> map2;
-    
-    public static void union(Map<String, Integer> map1, Map<String, Integer> map2){
+    public static Map<String, String> intersect(Map<String, Integer> map1, Map<String, Integer> map2){
         Map<String, String> mapRes = new HashMap<>();
         int count1;
         int count2;
         for (Map.Entry<String, Integer> pair1 : map1.entrySet()) {
-            String str1 = pair1.getKey();
-            for (Map.Entry<String, Integer> pair2 : map2.entrySet()) {
-                String str2 = pair2.getKey();
-                if(str1.equals(str2)){
-                    count1 = map1.get(str1);
-                    count2 = map2.get(str2);
-                    mapRes.put(str1," "+count1+","+count2);
-                }                
-            }                      
+            if (map2.containsKey(pair1.getKey())){
+                count1 = map1.get(pair1.getKey());
+                count2 = map2.get(pair1.getKey());
+                mapRes.put(pair1.getKey(),count1+","+count2);                                
+            }          
         }
         System.out.println(mapRes);
-    }
-
-    @Override
-    public String toString() {
-        return "Words{" + '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
+        return mapRes;
+    }  
     
-    
-    
+    public static void intersectWord (String str1, String str2){
+        Map<String, Integer> res1 = map(str1);
+        Map<String, Integer> res2 = map(str2);
+        intersect(res1,res2);
+    }
 }
--- words/test/words/WordsTest.java
+++ words/test/words/WordsTest.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 import java.util.Map;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static words.Words.intersectWord;
 
 public class WordsTest {
-
+    
     @Test
-    public void testMain() {
-        System.out.println("Test of main method");
-        String[] args = null;
-        Words.main(args);
-    }
+    public  void main() {
+        intersectWord(str1,str2);
+    }   
+
 
     @Test
     public void testMap() {
@@ -27,14 +27,30 @@ public class WordsTest {
     }
 
     @Test
-    public void testUnion() {
-        System.out.println("Test of union method");
+    public void testIntersect() {
+        System.out.println("Test of intersect method");
         Map<String, Integer> map1 = new HashMap<>();
         Map<String, Integer> map2 = new HashMap<>();
         map1.put("aaa", 2);
         map1.put("bbb", 3);
         map2.put("ccc", 1);
         map2.put("aaa", 4);
-        Words.union(map1, map2);
+        Map<String, String> expResult = new HashMap<>();
+        expResult.put("aaa", "2,4");
+        assertEquals(expResult, Words.intersect(map1, map2));
     }    
+    
+    @Test
+    public void testIntersectWord() {
+        System.out.println("Test of intersectWord method");
+        String str1 = "aaa aaa bbb ccc";
+        String str2 = "bbb bbb ccc fff";
+        Words.intersectWord(str1, str2);
+    }
+    
+    public static final String str1 = "mam dad father mam father mam mam dad sis father mam father mam";
+    public static final String str2 = "mam dad father mam bob dad";   
+
 }
+
+
