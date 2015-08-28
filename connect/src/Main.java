db1ae0799455d6b87e3b0fd2401bb3a951c633f6
artem <artem.gladyshuk@gmail.com>
2015-08-28 16:42:04 +0300

    Links v3

--- connect/src/Main.java
+++ connect/src/Main.java
@@ -5,17 +5,26 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       HashMap<Integer, String> s = readFile(fileNumbersNames);
+        HashMap<Integer, String> s = readFile(fileNumbersNames);
         selectionPerson(s);
     }
 
     public static String fileNumbersNames = "D://list.txt";
     public static String fileExistingLinks = "D://links.txt";
+    public static String nameFirstPerson = null;
+    public static String nameSecondPerson = null;
+    public static String link = null;
+    public static int numberFirstPerson;
+    public static int numberSecondPerson;
 
     private static void  checkExists(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         if (!file.exists()) {
-            throw new FileNotFoundException(file.getName());
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -43,20 +52,25 @@ public class Main {
     }
 
     public static String selectionPerson (HashMap <Integer, String> listNames) throws IOException {
+        /*Ввод номера человека с клавиатуры*/
         System.out.println("Enter a number from 1 to "+listNames.size());
-        int numberFirstPerson = System.in.read()-48;
-        Random random = new Random();
-        System.out.println(numberFirstPerson);
-        String nameFirstPerson = null;
-        String nameSecondPerson = null;
-        String link = null;
-        int numberSecondPerson;
+        char x = (char)System.in.read();
+        numberFirstPerson = Character.getNumericValue(x);
+        /*Выбор имени человека по номеру, который мы ввели*/
         if(listNames.containsKey(numberFirstPerson)){
             nameFirstPerson = listNames.get(numberFirstPerson);
         }
-
         /*Проверка наличия ранее созданой связи в файле*/
+        checkExistsLinks();
+        /*Создание новой связи рандомно, если нет в файле*/
+        createNewLink(listNames);
+        /*Вывод на экран созданной или уже существующей связи*/
+        System.out.println(link+" связаны судьбой!");
+        return link;
+    }
 
+    public static String checkExistsLinks() throws FileNotFoundException {
+        checkExists(fileExistingLinks);
         String links = readFileConnection(fileExistingLinks); // открываем файл
         String arrayLink [] = links.split("\t"); // в него ранее записывалось так: связь имя1, имя2 таб. По табу разбиваем на отдельные связи в массив
         for (int i = 0; i < arrayLink.length; i++) { // теперь каждую из них рассматриваем
@@ -67,18 +81,22 @@ public class Main {
                 break;
             }
         }
+        return link;
+    }
+
+    public static String createNewLink(HashMap <Integer, String> listNames){
+        Random random = new Random();
         if (nameSecondPerson == null) { //делаем проверку на то, нашли ли мы в файле связь, если нет, то значит и второе имя мы не записали, значит оно равно нул
             while (true) { //запускаем бесконечный цикл
                 numberSecondPerson = random.nextInt(listNames.size()) + 1; // берем рандомное число
                 if (numberSecondPerson != numberFirstPerson) { //если рандомное совпало с первым, тем что мы ввели с клавиатуры, то нужно другое рандомное число
-                    break; //выбрасывает из цикла, если рандом и нами введенное не совпали
+                    break; //выбрасывает из цикла, если рандом и нами введенное числа не совпали
                 }
             }
             nameSecondPerson = listNames.get(numberSecondPerson);
             link = (nameFirstPerson + ", " + nameSecondPerson);
             writeFileConnection(link);
         }
-        System.out.println(link+" связаны судьбой!");
         return link;
     }
 
