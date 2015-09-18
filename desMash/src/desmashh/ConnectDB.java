81888f9a5dab397be73c0b2b5cfb95ffd7861dcc
artem <artem.gladyshuk@gmail.com>
2015-09-18 14:51:28 +0300

    v1

---
+++ desMash/src/desmashh/ConnectDB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desmashh;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Artem
+ */
+public class ConnectDB {
+    public static Connection ConnectDB() throws ClassNotFoundException, 
+            SQLException {
+        Class.forName("com.mysql.jdbc.Driver");    
+           String url = "jdbc:mysql://localhost:3306/desMash";
+           String name = "root";
+           String pass = "";
+           Connection con = DriverManager.getConnection(url, name, pass);
+           return con;
+    }
+}
